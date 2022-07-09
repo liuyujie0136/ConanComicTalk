5eb6ce23ba754af78c93e85d7416583a8d1c725f
liuyujie0136 <68209438+liuyujie0136@users.noreply.github.com>
2022-07-09 19:25:35 +0800

    Update split.pl

--- split.pl
+++ split.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/env perl
 
+# cat index.html | perl -alne 'BEGIN{$idx=1; $sum=0}; open OUT, ">>$idx.html"; print OUT $_; if(/<hr \/>/){$sum+=1; if($sum>60){$idx+=1; $sum=0}}'
+
 open IN, "<index.html";
 
 $idx=1;
