6a904767b64f4b9c54eb91d23a244af8d85dffbc
Yujie Liu <liuyujie3201@outlook.com>
2022-07-10 17:54:55 +0800

    change image links

--- split.pl
+++ split.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 
-# cat index.html | perl -alne 'BEGIN{$idx=1; $sum=0}; open OUT, ">>$idx.html"; print OUT $_; if(/<hr \/>/){$sum+=1; if($sum>60){$idx+=1; $sum=0}}'
+# cat index.html | perl -alne 'BEGIN{$idx=1; $sum=0}; open OUT,">>$idx.html"; print OUT $_; if(/<hr \/>/){$sum+=1; if($sum>60){$idx+=1; $sum=0}}'
 
 open IN, "<index.html";
 
@@ -15,7 +15,7 @@ while (<IN>){
 		$sum+=1;
 		if($sum>60){
 			$idx+=1;
-			$sum=0
+			$sum=0;
 		}
 	}
 }
