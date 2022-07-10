05ff8d298f859e75a7df183e75d7a70c609b368e
Yujie Liu <liuyujie3201@outlook.com>
2022-07-10 19:43:43 +0800

    split pages

--- split.pl
+++ split.pl
@@ -2,20 +2,33 @@
 
 # cat index.html | perl -alne 'BEGIN{$idx=1; $sum=0}; open OUT,">>$idx.html"; print OUT $_; if(/<hr \/>/){$sum+=1; if($sum>60){$idx+=1; $sum=0}}'
 
-open IN, "<index.html";
+open IN, "<index.html.bak";
 
 $idx=1;
 $sum=0;
 
-while (<IN>){
-	open OUT, ">>$idx.html";
+open OUT, ">>$idx.html";
+print OUT "<!doctype html><html lang=\"zh-cn\"><head><meta charset=\"UTF-8\"><link rel=\"stylesheet\" type=\"text/css\" href=\"main.css\"><title>这是一整栋漫画讨论楼_第1页_柯哀分析文吧</title></head><body><div id=\"write\">\n<div class=\"page\">上一页&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp第 1 页&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href=\"2.html\" title=\"2.html\">下一页</a></div><hr />\n";
+
+while(<IN>){
 	print OUT $_;
 
 	if(/<hr \/>/){
 		$sum+=1;
-		if($sum>60){
+		if($sum>30){
+			$last=$idx-1;
+			$next=$idx+1;
+			print OUT "<div class=\"page\"><a href=\"$last.html\" title=\"$last.html\">上一页</a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp第 $idx 页&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href=\"$next.html\" title=\"$next.html\">下一页</a></div>\n</div></body></html>\n";
+			close OUT;
+
 			$idx+=1;
 			$sum=0;
+
+			$last=$idx-1;
+			$next=$idx+1;
+
+			open OUT, ">>$idx.html";
+			print OUT "<!doctype html><html lang=\"zh-cn\"><head><meta charset=\"UTF-8\"><link rel=\"stylesheet\" type=\"text/css\" href=\"main.css\"><title>这是一整栋漫画讨论楼_第$idx页_柯哀分析文吧</title></head><body><div id=\"write\">\n<div class=\"page\"><a href=\"$last.html\" title=\"$last.html\">上一页</a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp第 $idx 页&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href=\"$next.html\" title=\"$next.html\">下一页</a></div><hr />\n";
 		}
 	}
 }
